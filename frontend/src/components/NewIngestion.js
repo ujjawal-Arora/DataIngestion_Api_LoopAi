fc1cbaaf32d108701f90e452ea8e6de1eb7c86fd
Ujjawal arora <ujjawal.arora2004@gmail.com>
2025-06-05 11:04:24 +0530

    render changes

--- frontend/src/components/NewIngestion.js
+++ frontend/src/components/NewIngestion.js
@@ -14,7 +14,7 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 function NewIngestion() {
   const [ids, setIds] = useState('');
